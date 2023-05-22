34c86e25ded16b62db66bf7fe10522e4d7735d23
kjderevianko <kathrin.derevianko@gmail.com>
2023-05-22 21:08:23 +0200

    Updated CSS & JS

---
+++ script.js
@@ -0,0 +1,67 @@
+//Feature 1
+let currentDate = new Date();
+
+let h2 = document.querySelector("h2");
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let day = days[currentDate.getDay()];
+let date = currentDate.getDate();
+let month = months[currentDate.getMonth()];
+let hour = currentDate.getHours();
+let minutes = currentDate.getMinutes();
+
+h2.innerHTML = `${day}, ${month} ${date}, ${hour}:${minutes}`;
+
+//Feature 2
+
+function searchCity(event) {
+  event.preventDefault();
+
+  let searchInput = document.querySelector("#search-input");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${searchInput.value}`;
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", searchCity);
+
+//Feature 3
+function showCelsius(event) {
+  event.preventDefault();
+  let celsiusTemp = document.querySelector("#temperature");
+  celsiusTemp.innerHTML = 9;
+}
+
+let celsius = document.querySelector("#celsius");
+celsius.addEventListener("click", showCelsius);
+
+function showFahreinheit(event) {
+  event.preventDefault();
+  let fahreinheitTemp = document.querySelector("#temperature");
+  let temperature = fahreinheitTemp.innerHTML;
+  fahreinheitTemp.innerHTML = Math.round((temperature * 9) / 5 + 32);
+}
+
+let fahrenheit = document.querySelector("#fahrenheit");
+fahrenheit.addEventListener("click", showFahreinheit);
