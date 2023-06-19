cdb1700af07fdfd018f46ac6a7c4f44eccf570bb
kjderevianko <kathrin.derevianko@gmail.com>
2023-06-19 16:50:06 +0200

    Added Id description, updated JS and CSS

--- script.js
+++ script.js
@@ -67,7 +67,12 @@ let fahrenheit = document.querySelector("#fahrenheit");
 fahrenheit.addEventListener("click", showFahreinheit);
 
 function displayTemperature(response) {
-  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
 let apiKey = "2a2eaa51d996796495bf456e5b58adf4";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Haarlem&appid=${apiKey}&units=metric`;
